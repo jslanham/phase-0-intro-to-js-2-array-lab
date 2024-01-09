2f6ab50ec03d9d01df1a8a2445ae4cd88273c09f
Joseph Lanham <jslanham2020@gmail.com>
2024-01-09 16:26:10 -0500

    Array modification complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    let copyOfCats = [...cats, `${name}`];
+    return copyOfCats;
+}
+function prependCat(name){
+    let copyOfCats = [name, ...cats];
+    return copyOfCats;
+}
+function removeLastCat(){
+    let copyOfCats = cats.slice(0, cats.length - 1);
+    return copyOfCats
+}
+function removeFirstCat(){
+    let copyOfCats = cats.slice(1)
+    return copyOfCats
+}
+
+console.log(cats.length);
